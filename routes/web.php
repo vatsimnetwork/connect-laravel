2374169e467ec3e30d1a55d1c3b9f08c387c8ee5
Néstor Pérez <25409753+nepetadosmil@users.noreply.github.com>
2020-04-25 17:21:59 +0200

    Cleaned routes to show only Connect stuff

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 //--------------------------------------------------------------------------
-// Public Endpoint
+// You will need the following routes for Connect authentication
 //--------------------------------------------------------------------------
-Route::get('/', 'HomeController@index')->name('home');
-
-//--------------------------------------------------------------------------
-// Login Endpoint
-//--------------------------------------------------------------------------
-Route::middleware('guest')->group(function () {
-    Route::get('/login', 'Auth\LoginController@login')->name('login');
-    // Route::get('/validate', 'LoginController@validateLogin');
-});
-
-//--------------------------------------------------------------------------
-// Authenticated Endpoint
-//--------------------------------------------------------------------------
-Route::middleware('auth')->group(function () {
-    Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
-
-    // Routes here are protected by ConnectAuth.
-});
+Route::get('/login', 'Auth\LoginController@login')->middleware('guest')->name('login');   // Login
+Route::get('/logout', 'Auth\LoginController@logout')->middleware('auth')->name('logout'); // Logout
