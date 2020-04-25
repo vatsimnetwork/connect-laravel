407fd601e4bdebd7a2c1904dbc3deb249c7d8e95
Néstor Pérez <25409753+nepetadosmil@users.noreply.github.com>
2020-04-25 17:37:18 +0200

    Updated LoginController to contain only Connect stuff

--- app/http/controllers/auth/LoginController.php
+++ app/http/controllers/auth/LoginController.php
@@ -29,18 +29,20 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->provider = new VatsimOAuthController();
+        $this->provider = new VatsimOAuthController;
     }
 
     public function login(Request $request)
     {
-        if (! $request->has('code') || ! $request->has('state')) {
+        if (! $request->has('code') || ! $request->has('state')) { // User has clicked "login", redirect to Connect
             $authorizationUrl = $this->provider->getAuthorizationUrl(); // Generates state
             $request->session()->put('vatsimauthstate', $this->provider->getState());
-			return redirect()->away($authorizationUrl);
-        } else if ($request->input('state') !== session()->pull('vatsimauthstate')) {
-            return redirect()->route('home')->withError("Something went wrong, please try again (state mismatch).");
-        } else {
+	    	return redirect()->away($authorizationUrl);
+        }
+		else if ($request->input('state') !== session()->pull('vatsimauthstate')) { // State mismatch, error
+            return redirect('/')->withError("Something went wrong, please try again.");
+        }
+		else { // Callback (user has just logged in Connect)
             return $this->verifyLogin($request);
         }
     }
@@ -52,42 +54,35 @@ class LoginController extends Controller
                 'code' => $request->input('code')
             ]);
         } catch (IdentityProviderException $e) {
-            return redirect()->route('home')->withError("Something went wrong, please try again later.");
+            return redirect('/')->withError("Something went wrong, please try again later.");
         }
         $resourceOwner = json_decode(json_encode($this->provider->getResourceOwner($accessToken)->toArray()));
 
-        if (!
-            (isset($resourceOwner->data) &&
-	        isset($resourceOwner->data->cid) &&
-            isset($resourceOwner->data->personal->name_first) &&
-            isset($resourceOwner->data->personal->name_last) &&
-            isset($resourceOwner->data->personal->email) &&
-            $resourceOwner->data->oauth->token_valid === "true")
+		// Check if user has granted us the data we need
+        if (
+            ! isset($resourceOwner->data) ||
+	        ! isset($resourceOwner->data->cid) ||
+            $resourceOwner->data->oauth->token_valid !== "true"
         ) {
-            return redirect()->route('home')->withError("We need you to grant us all marked permissions");
+            return redirect('/')->withError("We need you to grant us all marked permissions");
         }
 
-        $account = $this->completeLogin($resourceOwner, $accessToken);
-
-        auth()->login($account, true);
-
-        return redirect()->intended(route('home'))->withSuccess('Login Successful');
+        $this->completeLogin($resourceOwner, $accessToken);
+        return redirect()->intended('/')->withSuccess('Login Successful');
     }
 
     protected function completeLogin($resourceOwner, $token)
     {
         $account = User::firstOrNew(['id' => $resourceOwner->data->cid]);
-        $account->fname = $resourceOwner->data->personal->name_first;
-        $account->lname = $resourceOwner->data->personal->name_last;
-        $account->email = $resourceOwner->data->personal->email;
-        if ($resourceOwner->data->oauth->token_valid) { // User has given us permanent access to updated data
+        if ($resourceOwner->data->oauth->token_valid === "true") { // User has given us permanent access to data
             $account->access_token = $token->getToken();
             $account->refresh_token = $token->getRefreshToken();
             $account->token_expires = $token->getExpires();
         }
 
         $account->save();
-
+        auth()->login($account, true);
+		
         return $account;
     }
 
@@ -95,6 +90,6 @@ class LoginController extends Controller
     {
         auth()->logout();
 
-        return redirect(route('home'))->withSuccess('You have been successfully logged out');
+        return redirect('/')->withSuccess('You have been successfully logged out');
     }
 }
